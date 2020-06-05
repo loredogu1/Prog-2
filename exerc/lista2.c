6d4e73232e7e3d2871e6a980b6c3aa33e8c08860
Guilherme Pereira Loredo <guilhermeloredo@usp.com>
2020-06-05 12:00:22 -0300

    up

--- exerc/lista2.c
+++ exerc/lista2.c
@@ -8,20 +8,30 @@
 
 int int main() {
     int nalunos[];
-    char nomeal;
+    char nomeal,;
     int nualuno;
     FILE*dados;
-     printf("Ver dados de alunos ja existentes?");
-     fopen= ;
+    printf("Criar turma?");
+    
+    printf("Ver dados de alunos ja existentes?");
+    fopen= ;
     printf("Digitir a quantidades de alunos na turma, com limite de 25");
     scanf("d", &nalunos);
-    if(){
+    if(nalunos[i]= NULL){
         printf("Aluno nao cadatrado");
     }
-    if(nalunos[i]= NULL){}
-
+     if(nalunos[i]= NULL)  {
+     printf("Erro na abertura do arquivo!");
+     return 1;
+  }
+    //nome do aluno
     printf("Informe o nome do Aluno");
-    
+     scanf("%s", nomeal);
+  //usando fprintf para armazenar a string no arquivo
+     fprintf(pont_arq, "%s", palavra);
+  //usando fclose para fechar o arquivo
+     fclose(pont_arq);
+
 
 
     return 0;
