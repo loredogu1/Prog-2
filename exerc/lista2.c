3d42699428083166551bd430603882db2d0daaab
Guilherme Pereira Loredo <guilhermeloredo@usp.com>
2020-06-04 23:16:22 -0300

    up

---
+++ exerc/lista2.c
@@ -0,0 +1,24 @@
+//Guilherme Pereira Loredo - USP Sao Carlos 
+//Codigo sobre Gerenciamentos de notas
+
+#include<stdio.h>
+#include <string.h>
+#include<conio.h>
+
+
+int int main() {
+    int nalunos[];
+    char nomeal;
+    int nualuno;
+    FILE*dados;
+    
+    printf("Digitir a quantidades de alunos na turma, com limite de 25");
+    scanf("d", &nalunos);
+
+
+    printf("Informe o nome do Aluno");
+    
+
+
+    return 0;
+}
