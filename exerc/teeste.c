667f38d1b2c16635aba33b6a75a333e00c058ce6
Guilherme Pereira Loredo <guilhermeloredo@usp.com>
2020-06-07 16:09:36 -0300

    up

---
+++ exerc/teeste.c
@@ -0,0 +1,22 @@
+
+#include<stdio.h>
+#include <string.h>
+#include<stdlib.h>
+#include 
+typedef struct{
+	char nome[15];
+	char snome[15]
+    int nota[4];
+}aluno;
+Aluno alunos [max alunos]
+
+int main() {
+    
+    
+    printf("Digitir a quantidades de alunos na turma, com limite de 25");
+    scanf("d", &nalunos);
+
+    
+
+    return 0;
+}
